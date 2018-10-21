d154c966ea338b826e2510f4bfdc393d67905fb7
nolanding <deekshachandvani@gmail.com>
2018-10-21 22:03:28 +0530

    added comments.

--- snake_ladder.py
+++ snake_ladder.py
@@ -17,27 +17,27 @@ for i in range(10):
 for i in range(9,-1,-1):
 	print(board[i])
 
-ladder={2:38,4:14,9:31,33:85,52:88,80:99}
-snake={56:15,62:57,92:53,51:11,98:8}
+ladder={2:38,4:14,9:31,33:85,52:88,80:99} #using dictionary to create a ladder (start point to end point)
+snake={56:15,62:57,92:53,51:11,98:8} # use dictionary to create a snake (start point end point(start point shoud be high for snake))
 print(ladder)
 print(snake)
 
 
-a=1
-b=1
-turn= 0
-
+a=1  #first player
+b=1 #second player
+turn= 0 #starts at player 1 and doing xor afer each iteration.
+flag=0
 while(1):
-	flag=0
+	 #kept this to decide the winner.
 	if(turn==1):
 		print("b's turn")
-		input("roll enter 1\n")
+		input("roll enter 1\n") # this is not necessary but have added this to make player aware that it is their turn.
 	else:
 		print("a's turn")
-		input("roll enter 1\n")	
-	dice=random.randint(1,6)
+		input("roll enter 1\n")	# this is not necessary but have added this to make player aware that it is their turn.
+	dice=random.randint(1,6) # this gives number on dice.
 	print(dice)
-	if(turn==0):
+	if(turn==0):   #checks the condition for player 1
 		if(a+dice<=100):
 			a+=dice
 			if(a  in ladder):
@@ -47,7 +47,7 @@ while(1):
 			if(a==100):
 				flag=1
 				break
-	else:					
+	else:				 #checks the condition for player 2.	
 		if(b+dice<=100):
 			b+=dice
 			if(b in ladder):
@@ -58,10 +58,10 @@ while(1):
 				flag=2
 				break		
 	turn^=1		
-	print(a,"a")
+	print(a,"a") #after every player prints player 1 and player 2 result
 	print(b,"b")
-print("winner id")	
+print("winner is ", end="")	#final winner of the game.
 if(flag==1):
-	print("dee")
+	print("a")
 elif(flag==2):
-	print("dhilu")		
+	print("b")		
